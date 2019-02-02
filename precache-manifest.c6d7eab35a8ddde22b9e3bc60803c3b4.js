bbd35b32d25ef26db2b585e04b71d36c9d6fed26
Seun Adetunji <oluwaseunus@gmail.com>
2019-02-02 05:30:40 +0100

    Updates

---
+++ precache-manifest.c6d7eab35a8ddde22b9e3bc60803c3b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f41d346ce9ebc121a229",
+    "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
+  },
+  {
+    "revision": "63953cbde622cb9cd099",
+    "url": "/pomodoro-clock/static/js/main.63953cbd.chunk.js"
+  },
+  {
+    "revision": "98d0031bc6c075977c1b",
+    "url": "/pomodoro-clock/static/js/1.98d0031b.chunk.js"
+  },
+  {
+    "revision": "63953cbde622cb9cd099",
+    "url": "/pomodoro-clock/static/css/main.12c848d3.chunk.css"
+  },
+  {
+    "revision": "6a29c5e2f8aec8bb250349e35acdbb50",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
